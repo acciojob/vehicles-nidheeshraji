d7ca55aca83b5abe4dd70b4f1026f98645601e61
nidheesh <nidhin@Dilips-iMac.local>
2023-01-21 21:54:45 +0530

    sol

--- src/main/java/com/driver/WaterVehicle.java
+++ src/main/java/com/driver/WaterVehicle.java
@@ -2,23 +2,28 @@ package com.driver;
 
 public interface WaterVehicle {
     public String getVehicleName();
+
     public int getVehicleCapacity();
-}
-class Boat implements WaterVehicle {
 
-    private String name;
-    private int capacity;
+    public class Boat implements WaterVehicle {
 
+        private String name;
+        private int capacity;
 
+        public Boat(String name, int capacity) {
+            this.name = name;
+            this.capacity = capacity;
+        }
 
-    @Override
-    public String getVehicleName() {
-        return this.name;
-    }
+        @Override
+        public String getVehicleName() {
+            return this.name;
+        }
 
-    @Override
-    public int getVehicleCapacity() {
-        return this.capacity;
+        @Override
+        public int getVehicleCapacity() {
+            return this.capacity;
+        }
     }
 }
 
